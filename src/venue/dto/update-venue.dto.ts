b6171aa3e030451428d68ad17845c106f569c42d
biancakimi <bibi.kimi09@gmail.com>
2026-09-17 21:37:25 -0300

    fix(GOL-81): updateaddress arrumado

--- src/venue/dto/update-venue.dto.ts
+++ src/venue/dto/update-venue.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateVenueDto } from './create-venue.dto';
+import { ValidateNested } from 'class-validator';
+import { UpdateAddressDto } from '../../address/dto/update-address.dto';
+import { Type } from 'class-transformer';
 
-export class UpdateVenueDto extends PartialType(CreateVenueDto) {}
+
+export class UpdateVenueDto extends PartialType(
+    OmitType(CreateVenueDto, ['address'] as const),
+) {
+    @ValidateNested()
+    @Type(() => UpdateAddressDto)
+    address?: UpdateAddressDto;
+    
+}
