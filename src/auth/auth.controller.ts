3555fe1e43ae187e29cdb2b5995aa08eb1fd2525
GustavoBaptistella <gustavobocampo@gmail.com>
2026-09-23 09:09:03 -0300

    feat(auth): implementa AuthService e AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK) // Retorna 200 em vez de 201 (padrão do POST) pois é só uma consulta/autenticação
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto.email, loginDto.password);
+  }
+}
